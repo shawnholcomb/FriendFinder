2eb243f0be3106c932f0c3677eae6ffad1ce5f54
Shawn Holcomb <sholcmb@gmail.com>
2019-03-15 16:01:30 -0500

    updated file structures

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('assets'))
+
 require("./app/routes/apiRoutes")(app);
 require("./app/routes/htmlRoutes")(app);
 
