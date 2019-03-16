3c11a451325ad2c3adf6ab5873d865ba583354c0
Shawn Holcomb <sholcmb@gmail.com>
2019-03-15 20:18:02 -0500

    completed server-side logic to determine friend matches; ready to deploy

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -2,15 +2,43 @@ var friendsArray = require("../data/friends");
 
 module.exports = function (app) {
   app.get("/api/friends", function (req, res) {
-    res.json(friends);
+    res.json(friendsArray);
   });
 
   app.post("/api/friends", function (req, res) {
-    var newFriend = req.body
+    var newFriend = req.body;
+    var friendMatch;
+    var scoreDiff = [];
+    var totalDiffArr = [];
 
+    // functions used for calculations
 
+    function sum(arr) {
+      return arr.reduce(function (a, b) {
+        return a + b
+      }, 0);
+    }
 
-    res.json(friendsArray[5]);
+    function min(arr) {
+      return Math.min.apply(null, arr);
+    }
+
+    // Calc differences between scores
+    friendsArray.forEach(function (friend) {
+      for (var i = 0; i < friend.scores.length; i++) {
+        scoreDiff.push(Math.abs(newFriend.scores[i] - friend.scores[i]))
+      }
+      var totalDiff = sum(scoreDiff);
+      totalDiffArr.push(totalDiff);
+      scoreDiff = [];
+      var totalDiff = 0;
+    });
+
+    // Determine matching superhero friend
+    var matchIndex = totalDiffArr.indexOf(min(totalDiffArr))
+    friendMatch = friendsArray[matchIndex];
+
+    res.json(friendMatch);
     friendsArray.push(newFriend);
   });
 };
